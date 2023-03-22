ea109849b9c7a0fb709540fe73bbbe10e6881ff5
ViniGB <viniciusemog@hotmail.com>
2023-03-22 18:01:27 -0300

    refactor: altera regex mastercard
    Signed-off-by: ViniGB <viniciusemog@hotmail.com>

--- src/utils/cardTypes.js
+++ src/utils/cardTypes.js
@@ -18,7 +18,7 @@ export const CARD_TYPES = [
     displayName: 'Mastercard',
     type: 'mastercard',
     format: DEFAULT_CARD_FORMAT,
-    startPattern: /^(5[1-5]|677189)|^(222[1-9]|2[3-6]\d{2}|27[0-1]\d|2720)/,
+    startPattern: /^(5[1-5]|677189|223115)|^(222[1-9]|2[3-6]\d{2}|27[0-1]\d|2720)/,
     gaps: [4, 8, 12],
     lengths: [16],
     code: {
